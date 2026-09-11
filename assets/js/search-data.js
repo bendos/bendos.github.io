180d747190a3e9f10a5f938c2ab0cdbfbf788870
bendos <benoit.dostie@gmail.com>
2026-09-11 19:08:57 +0000

    Deploying to gh-pages from @ bendos/bendos.github.io@7bbf21062f297e59d3c244799620e38ac3e9d875 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,19 +9,19 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-publications",
-          title: "publications",
-          description: "in reverse chronological order, split by publication type.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/publications/";
-          },
-        },{id: "nav-current-work",
+  },{id: "nav-current-work",
           title: "current work",
           description: "working papers and work in progress.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/current_work/";
+          },
+        },{id: "nav-publications",
+          title: "publications",
+          description: "in reverse chronological order, split by publication type.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
           },
         },{id: "post-a-post-with-plotly-js",
         
